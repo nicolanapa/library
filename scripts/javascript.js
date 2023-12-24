dd19158db76b2ef8f085b79409aa2f7aea2a9c23
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2023-12-24 18:03:03 +0100

    Added code

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,4 @@
-let myLibrary = ["Harry Potter by Rowling, 300 pages, read", "Jelson by Edwards, 210 pages, read",  "Jelson by Edwards, 210 pages, read", "Jelson by Edwards, 210 pages, read", "Jelson by Edwards, 210 pages, read", "Jelson by Edwards, 210 pages, read", "Jelson by Edwards, 210 pages, read"];
+let myLibrary = ["Harry Potter by Rowling, 300 pages, read", "Jelson by Edwards, 210 pages, read"];
 function Book(title, autor, pages, read) {
     this.title = title;
     this.autor = autor;
@@ -10,6 +10,11 @@ function Book(title, autor, pages, read) {
 }
 
 function addBookToLibrary() {
+    let bookTitle = document.getElementById("#title").value;
+    let bookAuthor = document.getElementById("#author").value;
+    let bookPages = document.getElementById("#pages").value;
+    let bookRead = document.getElementById("#read").value;
+    let newBook = new Book(bookTitle, bookAuthor, bookPages, bookRead);
 
 }
 
@@ -25,7 +30,19 @@ function addBookToSite() {
 addBookToSite();
 
 function readStatus() {
-    
+//Books already created read not read status
 }
-/*let ape = new Book("Api", "Gianni", "300 pages", "not read yet");
-addBookToLibrary(ape);*/
+
+let dialogNewBook = document.querySelector("#book-dialog");
+let buttonNewBook = document.querySelector("#newBookButton")
+buttonNewBook.addEventListener("click", function() {
+    dialogNewBook.showModal();
+});
+
+let formButton = document.querySelector("#form-button");
+formButton.addEventListener("click", function() {
+    addBookToLibrary();
+});
+
+//let bookForm = document.querySelector("#book-form");
+//let newBook =  new Book(bookForm#title, bookForm#author, bookForm#pages, bookForm#read);
